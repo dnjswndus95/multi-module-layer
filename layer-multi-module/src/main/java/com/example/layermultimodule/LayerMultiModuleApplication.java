b48691b07e8a879e8c9ec882ab6c368b1bba06c6
dnjswndus95 <32067312+dnjswndus95@users.noreply.github.com>
2024-03-28 09:46:51 +0900

    git init.

---
+++ layer-multi-module/src/main/java/com/example/layermultimodule/LayerMultiModuleApplication.java
@@ -0,0 +1,13 @@
+package com.example.layermultimodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LayerMultiModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LayerMultiModuleApplication.class, args);
+	}
+
+}
---
+++ layer-multi-module/src/test/java/com/example/layermultimodule/LayerMultiModuleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.layermultimodule;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LayerMultiModuleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
